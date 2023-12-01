61247d8602903e9e61e31d8bed5954ec31d08c4f
Bartłomiej Strama <kontakt@bstrama.com>
2023-12-01 12:28:23 +0000

    day 1 completed

---
+++ src/day1.rs
@@ -0,0 +1,69 @@
+use std::collections::HashMap;
+use crate::Part;
+
+pub fn main(part: Part) {
+    let input = include_str!("../assets/day1-input");
+
+    let word_digit_map = get_word_num_map();
+    let mut results_sum: u32 = 0;
+
+    for line in input.split('\n') {
+        let reverted_line = line.chars().rev().collect::<String>();
+
+        let first_num = find_first_num(line, false, &word_digit_map, &part);
+        let second_num = find_first_num(&reverted_line, true, &word_digit_map, &part);
+
+        results_sum += (first_num * 10 + second_num) as u32;
+    }
+
+    println!("Day 1 {} part result is: {}", part, results_sum);
+}
+
+fn find_first_num<'a>(line: &str, is_reversed: bool, word_digit_map: &HashMap<&'a str, u8>, part: &Part) -> u8 {
+    let mut currently_checked = String::new();
+
+    for char in line.chars() {
+        if let Some(digit) = char.to_digit(10) {
+            return digit as u8;
+        }
+        if let Part::SECOND = part {
+            currently_checked += &char.to_string();
+            if let Some(num) = find_in_map(currently_checked.clone(), is_reversed, word_digit_map) {
+                return num
+            }
+        }
+    }
+
+    unreachable!()
+}
+
+fn find_in_map(mut text: String, is_reversed: bool, map: &HashMap<&str, u8>) -> Option<u8> {
+    if is_reversed {
+        text = text.chars().rev().collect::<String>();
+    }
+
+    for (key, value) in map {
+        if text.contains(key) {
+            return Some(*value);
+        }
+    }
+
+    None
+}
+
+fn get_word_num_map<'a>() -> HashMap<&'a str, u8> {
+    let mut map = HashMap::new();
+
+    map.insert("zero", 0);
+    map.insert("one", 1);
+    map.insert("two", 2);
+    map.insert("three", 3);
+    map.insert("four", 4);
+    map.insert("five", 5);
+    map.insert("six", 6);
+    map.insert("seven", 7);
+    map.insert("eight", 8);
+    map.insert("nine", 9);
+
+    map
+}
---
+++ src/main.rs
@@ -0,0 +1,57 @@
+mod day1;
+
+use std::fmt::{Display, Formatter};
+use std::io;
+use std::io::Write;
+use std::process::exit;
+
+fn main() -> anyhow::Result<()> {
+    let mut day_input = String::new();
+    let mut part_input = String::new();
+
+    print!("Choose day number: ");
+    io::stdout().flush()?;
+
+    io::stdin().read_line(&mut day_input).expect("Failed to read input");
+    let day_nr = day_input.trim().parse::<u8>().expect("Failed to parse your input");
+
+    print!("First or second part (1|2): ");
+    io::stdout().flush()?;
+
+    io::stdin().read_line(&mut part_input).expect("Failed to read input");
+    let part = part_input.trim().parse::<u8>().expect("Failed to parse your input");
+    let part = match part {
+        1 => Part::FIRST,
+        2 => Part::SECOND,
+        _ => {
+            println!("Input must be 1 or 2");
+            exit(1);
+        }
+    };
+
+    match day_nr {
+        1 => {
+            day1::main(part);
+        }
+        _ => {
+            println!("Failed to found day nr.  {}", day_nr);
+            exit(1)
+        }
+    }
+
+    Ok(())
+}
+
+pub enum Part {
+    FIRST,
+    SECOND,
+}
+
+impl Display for Part {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::FIRST => write!(f, "first"),
+            Self::SECOND => write!(f, "second"),
+        }
+    }
+}
